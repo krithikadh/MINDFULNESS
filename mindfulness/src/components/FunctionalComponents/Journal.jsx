1c6ef9a283d8e7e29a6872d27630116d908200b3
Krithika <dhkrithika1114@gmail.com>
2025-02-28 10:32:02 +0530

    Journal page completed

--- mindfulness/src/components/FunctionalComponents/Journal.jsx
+++ mindfulness/src/components/FunctionalComponents/Journal.jsx
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import "../css/Journal.css";
 
-function Journal() {
+const Journal = () => {
+  const [entry, setEntry] = useState("");
+  const [entries, setEntries] = useState([]);
+
+  const handlePost = () => {
+    if (entry.trim() !== "") {
+      setEntries([entry, ...entries]);
+      setEntry("");
+    }
+  };
+
   return (
-      <p>Journal page</p>
+    <div className="journal-container">
+      <h1 className="journal">Reflect on your day's activities</h1>
+
+      <textarea
+        className="journal-input"
+        value={entry}
+        onChange={(e) => setEntry(e.target.value)}
+        placeholder="Write here"
+      ></textarea>
+
+      <button className="post-button" onClick={handlePost}>
+        Post
+      </button>
+
+      <div className="journal-entries">
+        {entries.map((entry, index) => (
+          <div key={index} className="journal-entry">
+            <p>{entry}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   );
-}
+};
 
 export default Journal;
